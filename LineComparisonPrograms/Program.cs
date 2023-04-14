c5898125c26aa958a241deda0bcee977adf87031
javeedp <javeedpatayit@gmail.com>
2023-04-14 17:47:26 +0530

    [javeedp] added line comparison

---
+++ LineComparisonPrograms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Line Comparison Program");
+            Line line = new Line(1, 2, 3, 4);
+            line.Length();
+            Console.ReadLine();
+
+        }
+    }
+}
